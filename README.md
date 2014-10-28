a3a12d549f3854369a418d0aa4de213e76033e41
mattak <mattak.me@gmail.com>
2014-10-28 18:08:02 +0900

    video showing works

---
+++ README.md
@@ -0,0 +1,13 @@
+# Avid
+
+Android Video Server
+
+# install
+
+    bundle install
+
+# run
+
+    bundle rackup
+
+Then you can access http://localhost:1028 .
--- lib/app.rb
+++ lib/app.rb
@@ -1,9 +1,15 @@
-require "sinatra/base"
+require 'sinatra/base'
+require 'slim'
 
 module Avid
   class App < Sinatra::Base
-    get '/' do
-      "hello"
+
+    VIDEO_ROOT = File.expand_path(File.dirname(__FILE__))
+    set :public_dir, File.dirname(__FILE__) + '/../public'
+
+    get '/:videoid' do
+      @videoid = params[:videoid]
+      slim :index
     end
   end
 end
